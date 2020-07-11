e2eab346748afdc21d2490bc2da1a2b1d1b8ce40
vpathalam <vpathalam@gmail.com>
2020-07-11 02:42:54 -0400

    initial commit soundboard, nav

---
+++ Soundboard/Soundboard/SoundListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SoundListViewController.swift
+//  Soundboard
+//
+//  Created by Vikram Pathalam on 7/11/20.
+//  Copyright © 2020 Vikram Pathalam. All rights reserved.
+//
+
+import UIKit
+
+class SoundListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
